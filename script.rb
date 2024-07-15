59c4622b4480f342c0dbc315eba6c3e3eb4ef55e
Kelechi125 <kelechi125@yahoo.com>
2024-07-14 19:52:07 -0500

    Created two variables for user input

---
+++ script.rb
@@ -0,0 +1,11 @@
+# Get user input of a string and a number and store them in variables
+# Create a function that will utilize these variables
+# Split the string into an array so we can loop over each letter
+
+string = gets.chomp
+number = gets.chomp.to_i
+alphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+
+def caeser_cipher
+  split_string = string.split("")
+end
