25f1158c54b5e190b86b60fb40a5311cfdaf6a01
Kelechi125 <kelechi125@yahoo.com>
2024-07-16 19:06:53 -0500

    Changed the letters variable from an array to a string; inside method, called the rotate method to shift the letters in the string down

--- script.rb
+++ script.rb
@@ -1,11 +1,16 @@
 # Get user input of a string and a number and store them in variables
 # Create a function that will utilize these variables
 # Split the string into an array so we can loop over each letter
+# Call the map function on the array of characters
 
 string = gets.chomp
 number = gets.chomp.to_i
-alphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+#upcase_letters = ("A".."Z").to_a
+letters = ("a".."z").to_a.join
 
 def caeser_cipher
-  split_string = string.split("")
+  new_string = string.chars.rotate(number).join
+  string.tr(letters, new_string_string)
 end
+
+caeser_cipher()
