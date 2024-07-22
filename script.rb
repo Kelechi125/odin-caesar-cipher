f350e238d0dc4d6b49128819e61b04d47ae3165f
Kelechi125 <kelechi125@yahoo.com>
2024-07-22 09:54:10 -0500

    Built caesar_cipher solution to transform the letters of a string down the alphabet a certain number of times

--- script.rb
+++ script.rb
@@ -2,41 +2,28 @@ require "pry-byebug"
 
 # Get user input of a string and a number and store them in variables
 # Create a function that will utilize these variables
-# Need to maintain the spaces and wrap from "z" to "a"
+# Need to maintain the spaces and punctuation and wrap from "z" to "a"
 # Create a variable with all the punctuation
 # Split the string into an array of characters
-# 
 
-#upcase_letters = ("A".."Z").to_a
-
-=begin def caeser_cipher |string|
-  puts "Write a string: "
+def caesar_cipher 
+  puts "Write something: "
   string = gets.chomp
 
   puts "Input a number: "
   number = gets.chomp.to_i
 
+  upcase_letters = ("A".."Z").to_a
+  upcase_encryptor = Hash[upcase_letters.zip(upcase_letters.rotate(number))]
 
-  end
+  downcase_letters = ("a".."z").to_a
+  downcase_encryptor = Hash[downcase_letters.zip(downcase_letters.rotate(number))]
+
+  encryptor = upcase_encryptor.merge(downcase_encryptor)
+
+  string.chars.map {|char| encryptor.fetch(char, char)}
 end
-=end
 
-# puts caeser_cipher()
-
-puts "Write a string: "
-string = gets.chomp
-
-puts "Input a number: "
-number = gets.chomp.to_i
-
-upcase_letters = ("A".."Z").to_a
-
-downcase_letters = ("a".."z").to_a
-
-alphabet = upcase_letters + downcase_letters
-
-punctuation = " `~!@#$%^&*()-_+=|?/>.<,\]}[{".split("")
-
-split_string = string.chars
+p caesar_cipher().join
 
 
